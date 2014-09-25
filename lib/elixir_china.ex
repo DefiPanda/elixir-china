7578530274f93bd9632bf274204dbe453ac0d189
jw2013 <zhew@live.unc.edu>
2014-09-25 02:55:46 -0700

    remove unused code

--- lib/elixir_china.ex
+++ lib/elixir_china.ex
@@ -4,7 +4,6 @@ defmodule ElixirChina do
   # See http://elixir-lang.org/docs/stable/elixir/Application.html
   # for more information on OTP Applications
   def start(_type, _args) do
-  	:ets.new :session, [:named_table, :public, read_concurrency: true]
     ElixirChina.Supervisor.start_link
   end
 end
