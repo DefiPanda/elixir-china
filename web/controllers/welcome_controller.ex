2639bb07334e2179a4f025020a87c6cf633d708f
jw2013 <zhew@live.unc.edu>
2014-09-26 02:21:21 -0700

    use session id also on index route

--- web/controllers/welcome_controller.ex
+++ web/controllers/welcome_controller.ex
@@ -1,7 +1,8 @@
 defmodule ElixirChina.WelcomeController do
+  import ElixirChina.ControllerUtils
   use Phoenix.Controller
 
   def index(conn, _params) do
-    render conn, "index"
+    render conn, "index", user_id: get_session(conn, :user_id)
   end
 end
