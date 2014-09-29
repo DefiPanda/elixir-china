24e4d5df6a5fe590627f7e28e67ae9bde01c421b
lidåshuång <ldshuang@gmail.com>
2014-09-29 08:51:02 +0800

    require 1.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ElixirChina.Mixfile do
   def project do
     [ app: :elixir_china,
       version: "0.0.1",
-      elixir: "~> 1.0.0-rc1",
+      elixir: "~> 1.0.0",
       elixirc_paths: ["lib", "web"],
       deps: deps ]
   end
