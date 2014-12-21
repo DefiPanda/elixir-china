90d8ce4ee4fe1274ba7d15b75967931aadcc045f
jw2013 <zhew@live.unc.edu>
2014-12-20 17:04:58 -0800

    upgrade elixir version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ElixirChina.Mixfile do
   def project do
     [ app: :elixir_china,
       version: "0.0.1",
-      elixir: "~> 1.0.0",
+      elixir: "1.0.2",
       elixirc_paths: ["lib", "web"],
       deps: deps ]
   end
