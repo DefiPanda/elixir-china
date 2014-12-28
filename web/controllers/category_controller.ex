9e253a4728be6758928d212707cb0cd46e7ad6ff
jw2013 <zhew@live.unc.edu>
2014-12-27 23:37:58 -0500

    Add pagination for posts display
    Add pagination for category posts
    
    Improve UI of category index page

--- web/controllers/category_controller.ex
+++ web/controllers/category_controller.ex
@@ -4,18 +4,44 @@ defmodule ElixirChina.CategoryController do
   alias ElixirChina.Post
   alias ElixirChina.Category
 
-  def index(conn, _params) do
+  @posts_per_page 10
+
+  def index(conn, %{"page" => page}) do
     render conn, "index", categories: get_categories(),
-                      posts: Repo.all(from p in Post, where: true, order_by: [{:desc, p.time}], preload: :user),
+                      posts: get_posts_by_page(page),
+                      pages: Repo.one(from p in Post, select: count(p.id)) / @posts_per_page |> Float.ceil,
+                      page: page,
+                      show_post: true,
+                      user_id: get_session(conn, :user_id)
+  end
+
+  def index(conn, %{}) do
+    index(conn, %{"page" => "1"})
+  end
+
+  def show(conn, %{"id" => id, "page" => page}) do
+    render conn, "show", categories: get_categories(),
+                      posts: get_posts_by_page_and_category(page, id),
+                      category: String.to_integer(id),
+                      pages: Repo.one(from p in Post, where: p.category_id == ^String.to_integer(id), select: count(p.id))
+                             / @posts_per_page |> Float.ceil,
+                      page: page,
+                      show_post: true,
                       user_id: get_session(conn, :user_id)
   end
 
   def show(conn, %{"id" => id}) do
-	  query = from p in Post, where: p.category_id == ^String.to_integer(id), order_by: [{:desc, p.time}], preload: :user
-    render conn, "show", categories: get_categories(),
-                      posts: Repo.all(query),
-                      category: String.to_integer(id),
-                      user_id: get_session(conn, :user_id)
+    show(conn, %{"id" => id, "page" => "1"})
+  end
+
+  defp get_posts_by_page(page) do
+    Repo.all(from p in Post, where: true, order_by: [{:desc, p.time}], limit: @posts_per_page,
+      offset: (String.to_integer(page) - 1) * @posts_per_page, preload: :user)
+  end
+
+  defp get_posts_by_page_and_category(page, id) do
+    Repo.all(from p in Post, where: p.category_id == ^String.to_integer(id), order_by: [{:desc, p.time}],
+      limit: @posts_per_page, offset: (String.to_integer(page) - 1) * @posts_per_page, preload: :user)
   end
 
   defp get_categories() do
